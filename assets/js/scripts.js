58977f45900191e5882b83cffe2d7a305c908c2b
teatall <teatall@foxmail.com>
2019-10-22 21:12:30 -0700

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,42 @@
 // custom js scripts
+
+(function ($) {
+
+	'use strict';
+
+	$(document).ready(function() {
+
+		// make img.img-card into cards and display alt text as caption
+		$("main img").each(function() {
+			$(this).wrap("<div class='card image-card-wrap d-block my-2 mx-0 mx-md-2 float-md-right'></div>");
+			$(this).addClass("card-img-top");
+			var imageCaption = $(this).attr("alt");
+			var imageSRC = $(this).attr("src");
+			if (imageCaption == null ) {
+				$("<div class='card-body'><small class='card-text'><i>Caption not available</i><a class='mg-popup ml-1 text-secondary' title='Caption not available' href='" + imageSRC + "'><i class='fas fa-search-plus'></i></a></small></div>").insertAfter(this);
+			} else {
+				$("<div class='card-body'><small class='card-text' role='note'>" + imageCaption + "<a class='mg-popup ml-1 text-secondary' href='" + imageSRC + "' title='" + imageCaption +"'><i class='fas fa-search-plus'></i></a></small></div>").insertAfter(this);
+			}
+		});			
+		
+		$('main').magnificPopup({
+			delegate: 'a.mg-popup',
+			type:'image',
+			gallery:{
+				enabled:true
+			},
+			callbacks: {
+				open: function() {
+					$('html, body').addClass('modal-open');
+				},
+				close: function() {
+					$('html, body').removeClass('modal-open');
+				}
+			},
+		});
+		
+		// more code here
+
+	});
+
+}(jQuery));
