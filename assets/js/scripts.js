499de08b669d06abcfe23009373555d67ebbae6e
teatall <7418648+teatall@users.noreply.github.com>
2019-10-23 11:39:15 -0700

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -35,6 +35,30 @@
 			},
 		});
 		
+		// enable typeahead search
+		  function initSearchBox() {
+		    var pages = new Bloodhound({
+		      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+		      // datumTokenizer: Bloodhound.tokenizers.whitespace,
+		      queryTokenizer: Bloodhound.tokenizers.whitespace,
+
+		      prefetch: baseurl + '/search.json'
+		    });
+
+		    $('#search-box').typeahead({
+		      minLength: 0,
+		      highlight: true
+		    }, {
+			name: 'pages',
+			display: 'title',
+			source: pages
+		      });
+
+		    $('#search-box').bind('typeahead:select', function (ev, suggestion) {
+		      window.location.href = suggestion.url;
+		    });
+		  }
+
 		// more code here
 
 	});
