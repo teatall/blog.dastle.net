0199195bed7ed2a569f4fc099559274856d45923
teatall <7418648+teatall@users.noreply.github.com>
2019-10-23 17:52:51 -0700

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -35,3 +35,34 @@ $(document).ready(function() {
 	// more code here
 
 });
+
+
+// initialise typeahead
+
+$(function () {
+
+  function initSearchBox() {
+    var pages = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+      // datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+
+      prefetch: '/search.json'
+    });
+
+    $('#search-box').typeahead({
+      minLength: 0,
+      highlight: true
+    }, {
+        name: 'pages',
+        display: 'title',
+        source: pages
+      });
+
+    $('#search-box').bind('typeahead:select', function (ev, suggestion) {
+      window.location.href = suggestion.url;
+    });
+  }
+
+  initSearchBox();
+});
